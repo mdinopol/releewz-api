60f9a4cf27d12d3af9a486146e9e5fe242610d46
mdinopol <ammd.softdev@gmail.com>
2023-09-01 02:00:01 +0800

    Apply code style changes

--- app/Enum/GameType.php
+++ app/Enum/GameType.php
@@ -8,9 +8,9 @@ enum GameType: string
 {
     use EnumToArray;
 
-    case REGULAR_SEASON = 'regular_season';
-    case QUALIFIER = 'qualifier';
+    case REGULAR_SEASON  = 'regular_season';
+    case QUALIFIER       = 'qualifier';
     case DIVISION_FINALS = 'division_finals';
-    case FINALS = 'finals';
-    case HEAD_TO_HEAD = 'head_to_head';
+    case FINALS          = 'finals';
+    case HEAD_TO_HEAD    = 'head_to_head';
 }
