bff1fa96f9115c95a2ee139feabddd62ddd69505
mdinopol <60090048+mdinopol@users.noreply.github.com>
2023-09-03 18:18:42 +0800

    Feature/crud (#3)
    achievements crud

---
+++ database/factories/AchievementFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Achievement>
+ */
+class AchievementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'        => fake()->word(),
+            'alias'       => fake()->word(),
+            'short'       => \Str::random(5),
+            'order'       => 0,
+            'is_range'    => false,
+            'description' => fake()->sentence(),
+        ];
+    }
+}
