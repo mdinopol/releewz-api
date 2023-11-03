70861f310f7df637a2f07a3c3e82ee3aa63e6091
mdinopol <ammd.softdev@gmail.com>
2023-11-03 18:56:30 +0800

    fix incorrect Collection namespace

--- app/Services/GameService.php
+++ app/Services/GameService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Enum\GameState;
 use App\Models\Game;
+use Illuminate\Database\Eloquent\Collection;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 class GameService
